23d27f7969361ca7b22588b82da78612b1e3e8be
Derrick Marfo <steammarfo111@gmail.com>
2025-06-24 11:29:56 +0000

    ReelProgress6

--- lib/screens/ReelsScreen/components/reelbox.dart
+++ lib/screens/ReelsScreen/components/reelbox.dart
@@ -40,11 +40,63 @@ class _ReelboxState extends State<Reelbox> {
     final height = MediaQuery.of(context).size.height;
     final width= MediaQuery.of(context).size.width;
     return  Container(
-      height: height*0.2,
-      width: height*0.2,
+      margin: const EdgeInsets.all(1),
+      height: height*0.15,
+      width: width*0.325,
       decoration: BoxDecoration(
         color: Colors.yellowAccent,
-        borderRadius: BorderRadius.circular(height*0.015),
+       
+        boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.7),blurRadius: 2,spreadRadius: 1)]
+      ),
+      child: VideoPlayer(_videoController),
+    );
+  }
+}
+
+class ReelboxRectangular extends StatefulWidget {
+  const ReelboxRectangular({super.key});
+
+  @override
+  State<ReelboxRectangular> createState() => _ReelboxRectangularState();
+}
+
+class _ReelboxRectangularState extends State<ReelboxRectangular> {
+ late VideoPlayerController _videoController;
+  @override
+  void initState() {
+    initializeVideoCrontroller();
+    super.initState();
+  }
+  Future <void> initializeVideoCrontroller()async{
+    _videoController=VideoPlayerController.networkUrl(Uri.parse('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'));
+    await _videoController.initialize();
+    _videoController.setLooping(true);
+    _videoController.setVolume(100);
+
+    if(!mounted) return;
+    _videoController.play();
+    setState(() {
+      
+    });
+  }
+
+   @override
+  void dispose() {
+   
+    _videoController.dispose();
+    super.dispose();
+  } 
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width= MediaQuery.of(context).size.width;
+    return  Container(
+      margin: const EdgeInsets.symmetric(horizontal: 1,vertical: 2),
+      height: height*0.3,
+      width: width*0.325,
+      decoration: BoxDecoration(
+        color: Colors.yellowAccent,
+       
         boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.7),blurRadius: 2,spreadRadius: 1)]
       ),
       child: VideoPlayer(_videoController),
--- lib/screens/ReelsScreen/reels.dart
+++ lib/screens/ReelsScreen/reels.dart
@@ -1,8 +1,6 @@
-import 'package:eco_world/screens/ReelsScreen/components/actionbar.dart';
 import 'package:eco_world/screens/ReelsScreen/components/reelbox.dart';
 import 'package:eco_world/screens/ReelsScreen/components/videobox.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class ReelsScreen extends StatefulWidget {
   const ReelsScreen({super.key});
@@ -12,85 +10,35 @@ class ReelsScreen extends StatefulWidget {
 }
 
 class _ReelsScreenState extends State<ReelsScreen> {
-  final PageController pageController = PageController();
-  final List<String> urls = [
-    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-    'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
-    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-  ];
-
-  /* late List<VideoPlayerController?> _controllers;
-  int currentIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _controllers = List.filled(urls.length, null);
-    _initializeController(currentIndex);
-  }
-
-  Future<void> _initializeController(int index) async {
-    if (_controllers[index] != null) return;
-
-    final controller = VideoPlayerController.networkUrl(Uri.parse(urls[index]));
-    await controller.initialize();
-    controller.setLooping(true);
-    controller.setVolume(100);
-    _controllers[index] = controller;
-
-    if (!mounted) return;
-
-    if (index == currentIndex) controller.play();
-    setState(() {});
-  }
-
-  void _onPageChanged(int newIndex) async {
-    _controllers[currentIndex]?.pause();
-    currentIndex = newIndex;
-
-    await _initializeController(newIndex);
-    if (newIndex + 1 < urls.length) _initializeController(newIndex + 1);
-    if (newIndex - 1 >= 0) _initializeController(newIndex - 1);
-
-    for (int i = 0; i < _controllers.length; i++) {
-      if (i < newIndex - 1 || i > newIndex + 1) {
-        _controllers[i]?.dispose();
-        _controllers[i] = null;
-      }
-    }
-
-    if (!mounted) return;
-
-    _controllers[newIndex]?.play();
-    setState(() {});
-  }
-
-  @override
-  void dispose() {
-    for (var controller in _controllers) {
-      controller?.dispose();
-    }
-    pageController.dispose();
-    super.dispose();
-  } */
+  bool videoboxtapped = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: /* PageView.builder(
-        controller: pageController,
-        scrollDirection: Axis.vertical,
-        onPageChanged: _onPageChanged,
-        itemCount: urls.length,
-        itemBuilder: (context, index) {
-          final controller = _controllers[index];
-          return controller != null
-              ? Videobox(videoController: controller)
-              : const Center(child: CircularProgressIndicator());
-        },
-      ), */
-    Videobox()
-    );
+        body: InkWell(
+            onTap: () {
+              setState(() {
+                videoboxtapped = true;
+              });
+            },
+            child: videoboxtapped ? const Videobox() : 
+            const Wrap(children: [ Column(
+               mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Reelbox(),
+                Reelbox(),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Reelbox(),
+                Reelbox(),
+              ],
+            ),
+            ReelboxRectangular()
+            
+            
+            ])));
   }
 }
